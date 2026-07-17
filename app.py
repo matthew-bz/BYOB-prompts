5d6f15af48424be693a07b920d2afebf97da59d7
matthew-bz <matthew.bohez@gmail.com>
2026-07-17 22:20:29 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import math
+import csv
+import random
+from random import shuffle
+import streamlit as st
+
+
+def generate_prompt():
+	df = pd.read_csv('BYOBook.csv', sep=",", header=1)
+
+	#print(df.head())
+	prompt_list = df['Card'].tolist()
+	#print(prompt_list)
+	def pick_prompt():
+    	return random.choice(prompt_list)
+    return pick_prompt()
+
+st.title("BYOBook: prompt generator")
+st.write("Prompts for the game BYOBook")
+
+prompt = pick_prompt()
+st.info(f"{prompt}")
+
+
+if st.button("Generate Another"):
+    st.rerun()
